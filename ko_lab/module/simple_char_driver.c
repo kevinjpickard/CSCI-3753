700ae6e92d2e0e2b7360ca749b0dcdbd8f3ca7ba
xenooparadox <kevinjpickard@gmail.com>
2015-09-27 18:46:34 -0600

    Implemented init and exit functions and struct definition

--- ko_lab/module/simple_char_driver.c
+++ ko_lab/module/simple_char_driver.c
@@ -6,6 +6,9 @@
 #include<linux/fs.h>
 #include<asm/uaccess.h>
 #define BUFFER_SIZE 1024
+#define MAJOR_NUMBER 261
+#define DEVICE_NAME simple_character_device
+
 
 static char device_buffer[BUFFER_SIZE];
 
@@ -47,6 +50,10 @@ struct file_operations simple_char_driver_file_operations = {
 
 	.owner   = THIS_MODULE,
 	/* add the function pointers to point to the corresponding file operations. look at the file fs.h in the linux souce code*/
+	.read		= simple_char_driver_read,
+	.write		= simple_char_driver_write,
+	.open		= simple_char_driver_open,
+	.release	= simple_char_driver_close,
 };
 
 static int simple_char_driver_init(void)
@@ -54,16 +61,16 @@ static int simple_char_driver_init(void)
 	/* print to the log file that the init function is called.*/
 	printk(KERN_ALERT "insdie %s function\n", __FUNCTION__);	
 	/* register the device */
-	register_chrdev(261, simple_character_device, *fops);
+	register_chrdev(MAJOR_NUMBER, DEVICE_NAME, &simple_char_fops);
 	return 0;
 }
 
 static int simple_char_driver_exit(void)
 {
 	/* print to the log file that the exit function is called.*/
-	pritnk(KERN_ALERT "inside %s function\n", __FUNCTION);
+	pritnk(KERN_ALERT "inside %s function\n", __FUNCTION__);
 	/* unregister  the device using the register_chrdev() function. */
-	unregister_chrdev(261, simple_character_device);
+	unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
 	return 0;
 }
 
