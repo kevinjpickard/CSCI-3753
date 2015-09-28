cc869e9eaca75e7d19ba5882eb329ba921accf12
xenooparadox <kevinjpickard@gmail.com>
2015-09-27 23:32:20 -0600

    Test function debugged and compiled. One last issue persists: when in Write mode, the read never exits, and will simply keep reading data in. Read function seems to work, however currently it only returns empty buffers. It is unclear if this is because the write function doesn't work so nothing can be written, or if the code is incorrect.

--- ko_lab/module/scd_test.c
+++ ko_lab/module/scd_test.c
@@ -2,18 +2,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 
 #define PATH "/dev/simple_character_device"
 #define BUFFER_SIZE 1024
 
 int main() {
-	char in, buffer[BUFFER_SIZE];
-	int file = open(PATH, O_RWDR);
+	char command, buffer[BUFFER_SIZE];
+	int file = open(PATH, O_RDWR);
 	printf("\n\n-----TEST FUNCTION FOR SIMPLE CHARACTER DEIVCE DRIVER-----\n");
-	printf("\nThis program tests the functionality of the simple character\n
-		device driver for CSCI 3753: Operating Systems. Please choose\n
-		one of the following options to continue:\n\n");
+	printf("\nThis program tests the functionality of the simple character\n\
+device driver for CSCI 3753: Operating Systems. Please choose\n\
+one of the following options to continue:\n\n");
 
 	printf("	----------------------------------------------\n");
 	printf("	R) Read from the simple character device.\n");
@@ -21,20 +21,23 @@ int main() {
 	printf("	E) Exit this test program, closing the device.\n\n");
 
 	while(1){
-		scanf('%s', $in);
-		switch(in){
-			case 'R' || 'r' :
+		scanf("%s", &command);
+		switch(command){
+			case 'R' :
+			case 'r' :
 				read(file, buffer, BUFFER_SIZE);
 				printf("Device Contents:\n	%s\n", buffer);
 				while(getchar() != '\n');
 					break;
-			case 'W' || 'w' :
+			case 'W' :
+			case 'w' :
 				printf("What would you like to store?\n");
 				scanf("%s", buffer);
 				write(file, buffer, BUFFER_SIZE);
-				while(getchar() != '\n);
+				while(getchar() != '\n');
 					break;
-			case 'E' || 'e' :
+			case 'E' :
+			case 'e' :
 				return 0;
 			default :
 				while(getchar() != '\n');
