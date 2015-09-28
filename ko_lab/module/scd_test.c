195b02660bf98da32e7019e11198cd53f7e6b150
xenooparadox <kevinjpickard@gmail.com>
2015-09-28 01:15:33 -0600

    Testing function complete, supports whitespace. Assignment complete. Cross your fingers a fucking buffer overflow doesn't happen...

--- ko_lab/module/scd_test.c
+++ ko_lab/module/scd_test.c
@@ -10,29 +10,33 @@
 int main() {
 	char command, buffer[BUFFER_SIZE];
 	int file = open(PATH, O_RDWR);
-	printf("\n\n-----TEST FUNCTION FOR SIMPLE CHARACTER DEIVCE DRIVER-----\n");
+	printf("\n\n-------TEST FUNCTION FOR SIMPLE CHARACTER DEIVCE DRIVER-------\n");
 	printf("\nThis program tests the functionality of the simple character\n\
 device driver for CSCI 3753: Operating Systems. Please choose\n\
-one of the following options to continue:\n\n");
-
-	printf("	----------------------------------------------\n");
-	printf("	R) Read from the simple character device.\n");
-	printf("	W) Write to the simple character device.\n");
-	printf("	E) Exit this test program, closing the device.\n\n");
+one of the following options (not case-sensitive) to continue:\n\n");
 
 	while(1){
-		scanf("%s", &command);
+
+		printf("	----------------------------------------------\n");
+		printf("	R) Read from the simple character device.\n");
+		printf("	W) Write to the simple character device.\n");
+		printf("	E) Exit this test program, closing the device.\n");
+		printf("	----------------------------------------------\n\n");
+		printf("Choice:");
+		scanf("%c", &command);
 		switch(command){
 			case 'R' :
 			case 'r' :
 				read(file, buffer, BUFFER_SIZE);
-				printf("Device Contents:\n	%s\n", buffer);
+				printf("Device Contents:\n");
+				printf("%s\n\n", buffer);
 				while(getchar() != '\n');
 					break;
 			case 'W' :
 			case 'w' :
-				printf("What would you like to store?\n");
-				scanf("%s", buffer);
+				printf("Data to store (> exits):");
+				scanf("%[^>]s", buffer);
+				printf("\n");
 				write(file, buffer, BUFFER_SIZE);
 				while(getchar() != '\n');
 					break;
@@ -41,6 +45,8 @@ one of the following options to continue:\n\n");
 				return 0;
 			default :
 				while(getchar() != '\n');
+					printf("Invalid entry, please choose one of the following options.\n\n");
+					break;
 		}
 	}
 	close(file);
